e53323d8ac14de9f8192291a2fc247aedc84e8d4
Joo614 <110887604+Joo614@users.noreply.github.com>
2022-10-24 00:30:12 +0900

    Create 최대값과 최솟값.java
    Restoration

---
+++ Level 2/01. 최댓값과 최솟값/최대값과 최솟값.java
@@ -0,0 +1,27 @@
+import java.util.Arrays; //이거 꼭 해주기
+class Solution {
+    public String solution(String s) {
+        //s -> 공백으로 구분된 숫자들
+        
+        //문자열을 공백으로 잘라서 배열에 넣고
+        //그 배열을 int 배열로 만들어서
+        //오름차순 하고
+        //맨 앞 요소랑 맨 뒤 요소 추출해서
+        //중간에 띄어쓰기 하나 넣고 반환
+        
+        String[] sArr = s.split(" ");		
+		
+		int[] sArrInt = new int[sArr.length];
+        
+		for(int i = 0; i < sArr.length; i++) {
+            sArrInt[i] = Integer.parseInt(sArr[i]);
+        }
+        
+        Arrays.sort(sArrInt);
+        
+        int min = sArrInt[0];
+        int max = sArrInt[sArrInt.length -1];
+		
+		return min + " " + max;
+    }
+}
