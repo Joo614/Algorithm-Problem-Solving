6cbdd95b725495db07453ec93fbd94ff64536a84
Joo614 <110887604+Joo614@users.noreply.github.com>
2022-11-04 02:41:53 +0900

    Update 음양 더하기.java

--- Level 1/19. 음양 더하기/음양 더하기.java
+++ Level 1/19. 음양 더하기/음양 더하기.java
@@ -1,1 +1,20 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        //signs의 i번째가 false면 absolutes의 i번째 요소에 -를 붙이고
+        //true면 그냥 아무것도 붙이지 않고
+        //새 배열에 넣어
+        //그리고 absolutes 배열 돌면서 하나씩 더해서 합을 리턴
+        
+        int sum = 0;
+        
+        for(int i = 0; i < signs.length; i++) {
+            if(signs[i] == false) {
+                sum -= absolutes[i];
+            } else {
+                sum += absolutes[i];
+            }
+        }
 
+        return sum;
+    }
+}
