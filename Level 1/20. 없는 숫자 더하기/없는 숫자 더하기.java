1c2f00328989e98ca249c877d8438aa5fdf26707
Joo614 <110887604+Joo614@users.noreply.github.com>
2022-11-05 16:28:55 +0900

    Update 없는 숫자 더하기.java

--- Level 1/20. 없는 숫자 더하기/없는 숫자 더하기.java
+++ Level 1/20. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,16 +1,19 @@
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 0;
+        int answer = 45; //0-9 까지의 합
         
-        int[] num = new int[]{0,1,2,3,4,5,6,7,8,9};
-        
-        for(int i = 0; i < numbers.length; i++) {
-            for(int j = 0; j < num.length; j++) {
-                if(numbers[i] != num[j]) {
-                    answer += numbers[i];
-                }
-            }
+        for(int num : numbers) {
+            answer -= num;
         }
+        
         return answer;
+    }
+}
+
+///////다른 풀이///////
+class Solution {
+    public int solution(int[] numbers){
+        
+        return 45 - Arrays.stream(numbers).sum();
     }
 }
