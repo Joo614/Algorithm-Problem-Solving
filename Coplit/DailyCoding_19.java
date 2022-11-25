d394472093cacc889ee89a14db808e7437a9efaf
Joo614 <110887604+Joo614@users.noreply.github.com>
2022-11-25 23:53:37 +0900

    Create DailyCoding_19.java

---
+++ Coplit/DailyCoding_19.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Solution { 
+	public String decryptCaesarCipher(String str, int secret) {
+    String alpha = "abcdefghijklmnopqrstuvwxyz";
+
+    String result = "";
+
+    for(int i = 0; i < str.length(); i++) {
+      if(str.charAt(i) == ' ') {
+        result = result + str.charAt(i);
+      } else {
+        int asis = alpha.indexOf(str.charAt(i));
+        int tobe = (asis - secret + alpha.length()) % alpha.length();
+        result = result + alpha.charAt(tobe);
+      }
+    }
+    return result;
+  }
+}
