cab3939df9567f9c1c85ee11a144594c17c545dd
Joo614 <110887604+Joo614@users.noreply.github.com>
2022-11-30 00:03:22 +0900

    Update 두 개 뽑아서 더하기.java

--- Programmers/Level 1/34. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ Programmers/Level 1/34. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -26,3 +26,22 @@ class Solution {
         return answer;
     }
 }
+
+/////// 다른 풀이 ///////
+
+import java.util.*;
+class Solution {
+    public int[] solution(int[] numbers) {
+        // But, HashSet을 이용하니 가독성은 좋지만, 속도면에서 느림
+        
+        Set<Integer> set = new HashSet<>();
+
+        for(int i = 0; i < numbers.length; i++) {
+            for(int j = i+1; j < numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);
+            }
+        }
+
+        return set.stream().sorted().mapToInt(Integer::intValue).toArray();
+    }
+}
