34ab2dbde68335c04238c9246a954fd4ad777aa2
Joo614 <110887604+Joo614@users.noreply.github.com>
2022-12-01 23:09:16 +0900

    Create DailyCoding_39.java

---
+++ Coplit/DailyCoding_39.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Solution { 
+	public int[] heapSort(int[] arr) {
+
+    PriorityQueue<Integer> heap = new PriorityQueue<Integer>();
+
+		for(int i = 0; i < arr.length; i++) {
+			heap.add(arr[i]);
+		}
+    
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = heap.poll();
+		}
+
+	  return arr;
+	}
+}
