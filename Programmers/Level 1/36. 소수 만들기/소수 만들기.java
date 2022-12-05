346822a4a24f2550756eb2e96e326305b24022a4
Joo614 <110887604+Joo614@users.noreply.github.com>
2022-12-05 23:40:03 +0900

    Update 소수 만들기.java

--- Programmers/Level 1/36. 소수 만들기/소수 만들기.java
+++ Programmers/Level 1/36. 소수 만들기/소수 만들기.java
@@ -44,9 +44,9 @@ class Solution {
         return answer;
     }
   
-    private boolean isPrime(int num) {
+    private boolean isPrime(int num) { // 소수인지 판별하는 메서드 만들기
         
-        for (int i = 2; i <= Math.sqrt(num); i++) {
+        for (int i = 2; i <= Math.sqrt(num); i++) { //끝까지 안해봐도 되고 중간까지만 해도 결과가 나옴
             // 나눠 떨어질 경우
             if (num % i == 0) {
                 return false;
