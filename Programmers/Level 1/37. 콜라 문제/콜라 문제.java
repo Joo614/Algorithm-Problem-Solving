56fe682866e418cbe6d130701eedfb24aad91a86
Joo614 <110887604+Joo614@users.noreply.github.com>
2022-12-07 00:16:01 +0900

    Update 콜라 문제.java

--- Programmers/Level 1/37. 콜라 문제/콜라 문제.java
+++ Programmers/Level 1/37. 콜라 문제/콜라 문제.java
@@ -1,1 +1,39 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        // 빈 병 개수가 a개보다 미민인 경우와 이상일 경우로 나누어 진행
+        // a개보다 미만일 경우 -- 리턴
+        // a개 이상일 경우 -- 빈병 개수를 a개로 나누어서 돌려받는 개수를 곱해주면 됨
+        // 나머지를 구하고 빈병의 개수인 n에 위의 값과 나머지 병을 더해주는 것을 while문 돌리면 됨
+      
+        int answer = 0;
+      
+        while(true) { // while문을 열어서
+            // 가지고 있는 빈 병 수가 a개보다 미만이면 return
+            if(n < a) {
+                break;
+            }
+            
+            // 가지고 있는 빈 병 수가 a개보다 크면 시작
+            answer += (n / a) * b; // answer에 n에서 a를 나눈 몫에 교환받는 콜라 수 b를 곱해서 넣어줌
+            
+            n = ((n/a)*b)+(n%a); // n을 answer에 넣어준 만큼과 a로 나눈 나머지를 더하여 구현
+        }
+        return answer;
+    }
+}
 
+/////// 다른 풀이 ///////
+
+class Solution {
+    public int solution(int a, int b, int n) {
+      
+        int answer = 0;
+
+        while (n >= a) { // 위와 다른 게 여기는 아예 이걸 이렇게 제한해줌
+            answer += b * (n / a);
+            n = b * (n / a) + n % a;
+        }
+
+        return answer;
+    }
+}
