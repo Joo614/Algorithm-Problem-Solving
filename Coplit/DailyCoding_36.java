eb769a98a101b38a85815ccdd1807f87d0554b77
Joo614 <110887604+Joo614@users.noreply.github.com>
2022-12-09 23:23:50 +0900

    Update DailyCoding_36.java

--- Coplit/DailyCoding_36.java
+++ Coplit/DailyCoding_36.java
@@ -2,15 +2,15 @@ public class Solution {
   public int[] insertionSort(int[] arr) {
 		
     for(int i = 1; i < arr.length; i++) {
-			int target = i;
+	int target = i;
 
-			while(target > 0 && arr[target-1] > arr[target]) {
-				int temp = arr[target-1];
-				arr[target-1] = arr[target];
-				arr[target] = temp;
-				target--;
-			}
-		}
-		return arr;
+	while(target > 0 && arr[target-1] > arr[target]) {
+		int temp = arr[target-1];
+		arr[target-1] = arr[target];
+		arr[target] = temp;
+		target--;
 	}
+      }
+    return arr;
+  }
 }
