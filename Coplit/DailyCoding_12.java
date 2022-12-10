0dc8d3625ae084027323b2674d8172e10aff172e
Joo614 <110887604+Joo614@users.noreply.github.com>
2022-12-10 23:46:36 +0900

    Create DailyCoding_12.java

---
+++ Coplit/DailyCoding_12.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution { 
+	public int[] reverseArr(int[] arr){
+    
+    if(arr.length == 0) return new int[]{};
+    
+    int[] head = Arrays.copyOfRange(arr, arr.length-1, arr.length);
+    int[] tail = reverseArr(Arrays.copyOfRange(arr, 0, arr.length-1));
+
+    int[] result = new int[head.length + tail.length];
+    System.arraycopy(head, 0, result, 0, head.length);
+    System.arraycopy(tail, 0, result, head.length, tail.length);
+
+    return result;
+	} 
+}
