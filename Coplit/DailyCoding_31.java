62ba7d074b21aa0b9258e98b57a9384bf1143d53
Joo614 <110887604+Joo614@users.noreply.github.com>
2022-12-28 21:15:50 +0900

    Create DailyCoding_31.java

---
+++ Coplit/DailyCoding_31.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Solution { 
+	public ArrayList<String> powerSet(String str) {
+
+		String deduplStr = deduplicated(str);
+		Stack<String> stack = new Stack<>();
+
+		ArrayList<String> result = new ArrayList<>();
+
+		result = pickOrNot(stack, 0, deduplStr, result);
+		Collections.sort(result);
+		return result;
+	};
+  
+	public String deduplicated(String str) {
+    String result = "";
+    for(int index = 0; index < str.length(); index++) {
+      if(str.indexOf(str.charAt(index)) == index) {
+        result += str.charAt(index);
+      }
+    }
+
+    String[] sorted = result.split("");
+    Arrays.sort(sorted);
+    result = String.join(",", sorted).replaceAll(",", "");
+    return result;
+  }	
+
+	public ArrayList<String> pickOrNot(Stack<String> stack, int idx, String subset, ArrayList<String> result) {
+
+    if(idx >= subset.length()) {
+      result.add(stack.toString()
+                .replaceAll("\\[", "")
+                .replaceAll("]", "")
+                .replaceAll(",", "")
+                .replaceAll(" ", ""));
+      return result;
+    } else {
+      stack.push(Character.toString(subset.charAt(idx)));
+      pickOrNot(stack, idx + 1, subset, result);
+
+
+      stack.pop();
+      pickOrNot(stack, idx + 1, subset, result);
+    }
+    return result;
+  }
+}
