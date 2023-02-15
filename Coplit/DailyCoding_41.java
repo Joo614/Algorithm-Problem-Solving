7787f1dca66a2803bf232e79afc11c7980309822
Joo614 <110887604+Joo614@users.noreply.github.com>
2023-02-15 21:15:50 +0900

    Update DailyCoding_41.java

--- Coplit/DailyCoding_41.java
+++ Coplit/DailyCoding_41.java
@@ -1,22 +1,22 @@
 public class Solution { 
 	public int binarySearch(int[] arr, int target) {
 
-		int left = 0;
-		int right = arr.length - 1;
+	int left = 0;
+	int right = arr.length - 1;
 
-		while (left <= right) {
-			int middle = (right + left) / 2;
+	while (left <= right) {
+		int middle = (right + left) / 2;
 
-			if (arr[middle] == target)
-        return middle;
+		if (arr[middle] == target)
+        		return middle;
 
-			if (target < arr[middle]) {
-				right = middle - 1;
-			}
-      else {
-				left = middle + 1;
-			}
+		if (target < arr[middle]) {
+			right = middle - 1;
 		}
-		return -1;
+      		else {
+			left = middle + 1;
+		}
+	}
+	return -1;
 	}
 }
