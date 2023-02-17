5c02ec7ef81f690fe44a923fce0f83941930c695
Joo614 <110887604+Joo614@users.noreply.github.com>
2023-02-17 23:56:10 +0900

    Update DailyCoding_30.java

--- Coplit/DailyCoding_30.java
+++ Coplit/DailyCoding_30.java
@@ -1,31 +1,31 @@
 public class Solution { 
 	public int rotatedArraySearch(int[] rotated, int target) {
-    int left = 0;
+    		int left = 0;
 		int right = rotated.length - 1;
 
 		while(left <= right) {
 			int middle = (right + left) / 2;
 
-		if(rotated[middle] == target) {
+			if(rotated[middle] == target) {
 				return middle;
-		}
+			}
 
-		if (rotated[left] < rotated[middle]) {
-      if (target < rotated[middle] && rotated[left] <= target) {
-      	 right = middle - 1;
-      }
-      else {
-       	left = middle + 1;
-      }
-    }
-    else {
+			if (rotated[left] < rotated[middle]) {
+      				if (target < rotated[middle] && rotated[left] <= target) {
+      	 				right = middle - 1;
+      				}
+      			else {
+       				left = middle + 1;
+      			}
+    		}
+    		else {
 			if (target <= rotated[right] && rotated[middle] < target) {
 				left = middle + 1;
 			}
-      else {
+      		else {
 				right = middle - 1;
 			}
-    }
+    		}
 	}
 	return -1;
   } 
