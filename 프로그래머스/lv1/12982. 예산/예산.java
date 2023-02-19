70837e7046297a0c8d63b8d03d81db6605edd7ef
Joo614 <110887604+Joo614@users.noreply.github.com>
2023-02-19 16:14:37 +0900

    [level 1] Title: 예산, Time: 0.61 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] d, int budget) {
+        // 최대한 많은 부서의 물품을 구매해 줄 수 있도록
+        // 신청한 금액만큼 모두 지원 - 더 적은 금액 주면 안됨
+        // 신청한 금액이 들어있는 배열 d / 예산
+        // 최대 몇개의 부서에 물품을 지원할 수 있는지
+        // 배열 길이는 1 <= d.length() <= 100
+
+        Arrays.sort(d);
+
+        int sum = 0; // 총 금액
+        int count = 0; // 몇 부서 지원할 수 있는지
+
+        Arrays.sort(d);
+
+        for(int i = 0; i < d.length; i++) {
+            sum += d[i];
+            if (sum > budget) {
+                count = i;
+                break;
+            }
+        }
+        if (sum <= budget) {
+            count = d.length;
+        }
+        System.out.println(count);
+        return count;
+    }
+}
