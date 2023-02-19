e7c824835ca55225d8e7684a227e6a4f7649438e
Joo614 <110887604+Joo614@users.noreply.github.com>
2023-02-19 21:23:32 +0900

    Update and rename 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java to Programmers/Level 2/04. 영어 끝말잇기/영어 끝말잇기.java

--- Programmers/Level 2/04. 영어 끝말잇기/영어 끝말잇기.java
+++ Programmers/Level 2/04. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,6 +1,13 @@
 import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
+        // n = 몇명이서 하는지 / words = 사람들이 계속 말한 단어
+        // 2<=n<=10 / n<=words<=100
+        // int[] answer = [가장 먼저 탈락하는 사람의 번호, 그 사람이 몇번째에 탈락하는지]
+        // 모든 단어는 알파벳 소문자
+        // 탈락자 안생기면 (겹치는 단어 없으면) return [0,0]
+        // 탈락자인 경우 -> 겹치는 단어 말 한 경우 / 앞에서 끝난 단어가 아닌 다른 단어로 시작했을 경우
+        
         int[] answer = new int[2];
         List<String> list = new ArrayList<>();
 
