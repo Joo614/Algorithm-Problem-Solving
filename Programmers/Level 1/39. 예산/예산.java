b7364daa6c27e268dd0cdddba599564618385ff9
Joo614 <110887604+Joo614@users.noreply.github.com>
2023-02-23 23:58:28 +0900

    Update 예산.java

--- Programmers/Level 1/39. 예산/예산.java
+++ Programmers/Level 1/39. 예산/예산.java
@@ -36,14 +36,15 @@ class Solution {
   public int solution(int[] d, int budget) {
       int answer = 0;
 
-        Arrays.sort(d);
+      Arrays.sort(d);
 
-        for (int i = 0; i < d.length; i++) {
-            budget -= d[i];
+      for (int i = 0; i < d.length; i++) {
+          budget -= d[i];
 
-            if (budget < 0) break;
-            answer++;
-        }
-        return answer;
+          if (budget < 0) break;
+          answer++;
+      }
+      
+      return answer;
   }
 }
