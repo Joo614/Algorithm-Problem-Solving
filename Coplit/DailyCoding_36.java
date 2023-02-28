727aed94dc092fead895361b63f48973db507af8
Joo614 <110887604+Joo614@users.noreply.github.com>
2023-02-28 23:52:09 +0900

    Update DailyCoding_36.java

--- Coplit/DailyCoding_36.java
+++ Coplit/DailyCoding_36.java
@@ -3,12 +3,12 @@ public class Solution {
 		
     for(int i = 1; i < arr.length; i++) {
 	int target = i;
-
+  
 	while(target > 0 && arr[target-1] > arr[target]) {
 		int temp = arr[target-1];
 		arr[target-1] = arr[target];
 		arr[target] = temp;
-		target--;
+		target--;  
 	}
       }
     return arr;
