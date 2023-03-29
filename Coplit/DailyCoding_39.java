42e79c3052aa16a91d62da85e5824fd9d5bbe19a
Joo614 <110887604+Joo614@users.noreply.github.com>
2023-03-29 23:52:19 +0900

    Update DailyCoding_39.java

--- Coplit/DailyCoding_39.java
+++ Coplit/DailyCoding_39.java
@@ -7,12 +7,12 @@ public class Solution {
 
 		for(int i = 0; i < arr.length; i++) {
 			heap.add(arr[i]);
-		}
+		}   
     
 		for(int i = 0; i < arr.length; i++) {
 			arr[i] = heap.poll();
 		}
-		
+		   
 	  return arr;
 	}
 }
