e5d1f381ff1d1d23973769a4893466378e34a0d5
Joo614 <110887604+Joo614@users.noreply.github.com>
2023-04-04 23:58:01 +0900

    Update DailyCoding_44.java

--- Coplit/DailyCoding_44.java
+++ Coplit/DailyCoding_44.java
@@ -2,16 +2,16 @@ package com.codestates.coplit;
 
 public class Solution { 
 	public int LSCS(int[] arr) {
-		int subArrSum = 0;
-		int max = Integer.MIN_VALUE;  
+		int subArrSum = 0; 
+		int max = Integer.MIN_VALUE;
 		
 		for(int i = 0; i < arr.length; i++) {
 			subArrSum = subArrSum + arr[i];
-			  
-			if(subArrSum > max)   
+			 
+			if(subArrSum > max)
 				max = subArrSum;
 
-			if (subArrSum < 0)  
+			if (subArrSum < 0)
 				subArrSum = 0;
 		}
 		return max;
