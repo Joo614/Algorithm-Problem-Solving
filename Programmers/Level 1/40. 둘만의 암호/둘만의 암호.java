ce5c05f419f0c859ac59f30f149c22811b30ec80
Joo614 <110887604+Joo614@users.noreply.github.com>
2023-04-07 21:15:50 +0900

    Update 둘만의 암호.java

--- Programmers/Level 1/40. 둘만의 암호/둘만의 암호.java
+++ Programmers/Level 1/40. 둘만의 암호/둘만의 암호.java
@@ -19,3 +19,33 @@ class Solution {
         return answer;
     }
 }
+
+/////// 다른 풀이 ///////
+
+import java.util.ArrayList;
+
+public class Solution {
+    public String solution(String s, String skip, int index) {
+        String answer = "";
+        ArrayList<Character> alpha = new ArrayList<>();
+        
+        for(int i=97; i<123;i++){
+            alpha.add((char)i);
+        }
+        
+        for (int i=0;i<skip.length();i++){
+            alpha.remove(Character.valueOf(skip.charAt(i)));
+        }
+        
+        for (int i=0; i<s.length();i++){
+            int idx = alpha.indexOf(s.charAt(i))+index;
+            
+            if(idx>=(26-skip.length())){
+                idx%=(26-skip.length());
+            }
+            char cha = alpha.get(idx);
+            answer+=cha;
+        }
+        return answer;
+    }
+}
