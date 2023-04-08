de41214d0a360e6ce33afc1e496f1157d572d983
Joo614 <110887604+Joo614@users.noreply.github.com>
2023-04-09 00:02:12 +0900

    Update 가장 가까운 같은 글자.java

--- Programmers/Level 1/41. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ Programmers/Level 1/41. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -1,23 +1,34 @@
 class Solution {
     public int[] solution(String s) {
-        int[] answer = new int[s.length()];
-        
-        for(int i=0;i<s.length();i++){
-            if(i !=0){
-                int idx = s.substring(0,i).lastIndexOf(s.charAt(i));
-                if(idx != -1){
-                    answer[i] = i-idx;
-                    
+        char[] ca = s.toCharArray();
+        int len = ca.length;
+        int[] answer = new int[len];
+        for(int i = 0; i < len; i++){
+            answer[i] = -1;
+            for(int j = 1; j <= i ; j++){
+                if(ca[i-j] == ca[i]){
+                    answer[i] = j;
+                    break;
                 }
-                else{
-                    answer[i] = idx;
-                }
-            }
-            else{
-                answer[i] = -1;
             }
         }
-        
+        return answer;
+    }
+}
+
+/////// 다른 풀이 ///////
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i=0; i<s.length();i++){
+            char ch = s.charAt(i);
+            answer[i] = i-map.getOrDefault(ch,i+1);
+            map.put(ch,i);
+        }
         return answer;
     }
 }
