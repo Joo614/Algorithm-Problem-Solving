39bfa708576ccfab2c29f53cd81093f8183d7416
Joo614 <110887604+Joo614@users.noreply.github.com>
2023-04-09 23:52:17 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 11.38 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,17 +1,13 @@
 class Solution {
     public String solution(int[] food) {
-        
-        StringBuilder sb = new StringBuilder();
-        
-        // food[0] : 물 
-        for(int i = 1; i < food.length; i++) {
-            int division = food[i] / 2;
-            
-            for(int j = 0; j < division; j++) {
-                sb.append(i);
+        String answer = "0";
+
+        for (int i = food.length - 1; i > 0; i--) {
+            for (int j = 0; j < food[i] / 2; j++) {
+                answer = i + answer + i; 
             }
         }
-        String answer = sb.toString() + "0" + sb.reverse().toString();
+
         return answer;
     }
 }
