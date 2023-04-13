d5fa9973e7c06e78f1e5985f2f7213ee2201e152
Joo614 <110887604+Joo614@users.noreply.github.com>
2023-04-13 23:58:58 +0900

    Update 덧칠하기.java

--- Programmers/Level 1/45. 덧칠하기/덧칠하기.java
+++ Programmers/Level 1/45. 덧칠하기/덧칠하기.java
@@ -1,5 +1,9 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
+        // 처음 인덱스 값부터 폼롤러를 돌리기 때문에 --> answer의 값 1 증가
+        // 배열의 값이 m의 값보다 커질때 --> answer의 값 1 증가
+        // ==> 덧칠해야하는 최소한의 수
+        
         int answer = 0;
         int now = section[0];
         int idx = 0;
