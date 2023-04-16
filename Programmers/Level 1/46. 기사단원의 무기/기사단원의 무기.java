499b0774328415f5f13e466497de43710eeb44fa
Joo614 <110887604+Joo614@users.noreply.github.com>
2023-04-16 23:50:29 +0900

    Update 기사단원의 무기.java

--- Programmers/Level 1/46. 기사단원의 무기/기사단원의 무기.java
+++ Programmers/Level 1/46. 기사단원의 무기/기사단원의 무기.java
@@ -18,7 +18,31 @@ class Solution {
             if (i * i == num) count++;
             else if (num % i == 0) count += 2;
         }
-
         return count;
+    }
+}
+
+/////// 다른 풀이 ////////
+
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 1;
+
+        for (int i = 2; i <= number; ++i) {
+            int count = 0;
+
+            for (int j = 1;  j * j <= i; ++j) {
+                if (i % j == 0) {
+                    count += (j * j == i) ? 1 : 2;
+                }
+
+                if (count > limit) {
+                     count = power;
+                    break;
+                }
+            }
+            answer += count;
+        }
+        return answer;
     }
 }
