558d72ff522af171e066a8997dfe15e0db563f85
Joo614 <110887604+Joo614@users.noreply.github.com>
2023-04-17 23:59:11 +0900

    Update 푸드 파이트 대회.java

--- Programmers/Level 1/42. 푸드 파이트 대회/푸드 파이트 대회.java
+++ Programmers/Level 1/42. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -11,3 +11,23 @@ class Solution {
         return answer;
     }
 }
+
+/////// 다른 풀이 ///////
+
+class Solution {
+    public String solution(int[] food) {
+        
+        StringBuilder sb = new StringBuilder();
+        
+        // food[0] : 물 
+        for(int i = 1; i < food.length; i++) {
+            int division = food[i] / 2;
+            
+            for(int j = 0; j < division; j++) {
+                sb.append(i);
+            }
+        }
+        String answer = sb.toString() + "0" + sb.reverse().toString();
+        return answer;
+    }
+}
