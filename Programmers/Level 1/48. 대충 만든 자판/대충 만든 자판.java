e5422dda28c1e0c2e8bc1022ce6d67e8e8442a02
Joo614 <110887604+Joo614@users.noreply.github.com>
2023-04-20 23:54:47 +0900

    Update 대충 만든 자판.java

--- Programmers/Level 1/48. 대충 만든 자판/대충 만든 자판.java
+++ Programmers/Level 1/48. 대충 만든 자판/대충 만든 자판.java
@@ -2,12 +2,12 @@ class Solution {
     public int[] solution(String[] keymap, String[] targets) {
         int[] answer = new int[targets.length];        
         
-        for(int i=0; i < targets.length; i++) {
+        for(int i=0; i < targets.length; i++) { //타겟 중 1개
             
-        	for(int j=0; j < targets[i].length(); j++) {
+        	for(int j=0; j < targets[i].length(); j++) { // 타켓의 문자
         		int idx = 101;
                 
-        		for(int k=0; k<keymap.length; k++) {
+        		for(int k=0; k<keymap.length; k++) { // keyMap 개수 비교
         			int index = keymap[k].indexOf(targets[i].charAt(j));
                     
         			if(index != -1) {
