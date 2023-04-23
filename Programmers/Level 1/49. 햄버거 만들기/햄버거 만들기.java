2c128f3ca7092b6ce6f4737cb0bcea2d6dff0fc9
Joo614 <110887604+Joo614@users.noreply.github.com>
2023-04-24 00:00:16 +0900

    Update 햄버거 만들기.java

--- Programmers/Level 1/49. 햄버거 만들기/햄버거 만들기.java
+++ Programmers/Level 1/49. 햄버거 만들기/햄버거 만들기.java
@@ -1,1 +1,29 @@
-s
+public class Solution {
+	
+	// 햄버거 만들기
+	// 빵(1), 야채(2), 고기(3), 빵(1)일 경우 햄버거 완성
+	
+	public static int solution(int[] ingredient) {
+		int answer = 0;
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 0; i < ingredient.length; i++) {
+			sb.append(ingredient[i]);
+			
+			if (sb.length() > 3 && sb.subSequence(sb.length() - 4, sb.length()).equals("1231")) {
+				sb.delete(sb.length() - 4, sb.length());
+				answer++;
+			}
+		}
+		
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] ingredient = {2, 1, 1, 2, 3, 1, 2, 3, 1};
+		
+		System.out.println(solution(ingredient));
+	}
+}
