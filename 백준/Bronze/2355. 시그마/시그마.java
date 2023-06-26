2052a044565f3ac9679cd030f552d711683b6709
Joo614 <110887604+Joo614@users.noreply.github.com>
2023-06-26 21:15:50 +0900

    [Bronze II] Title: 시그마, Time: 208 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Bronze/2355. 시그마/시그마.java
+++ 백준/Bronze/2355. 시그마/시그마.java
@@ -1,3 +1,6 @@
+// 가우스 덧셈 공식
+// n<m일 때, n부터 m까지의 정수를 모두 더한 값은
+// (n+m) * (m-n+1) / 2
 import java.util.Scanner;
 
 public class Main {
@@ -8,9 +11,11 @@ public class Main {
         long b = scan.nextLong();
         
         if (a < b) {
-            System.out.println((b * (b + 1)) / 2 - (a * (a - 1)) / 2);
+            //System.out.println((b * (b + 1)) / 2 - (a * (a - 1)) / 2);
+            System.out.println((a+b) * (b-a+1) / 2);
         } else if (b < a) {
-            System.out.println((a * (a + 1)) / 2 - (b * (b - 1)) / 2);
+            //System.out.println((a * (a + 1)) / 2 - (b * (b - 1)) / 2);
+            System.out.println((a+b) * (a-b+1) / 2);
         } else {
             System.out.println(a);
         }
