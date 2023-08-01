823e38052a365daa2c39bfd86df16abc62ee7419
Joo614 <110887604+Joo614@users.noreply.github.com>
2023-08-01 21:15:50 +0900

    [level 1] Title: 시저 암호, Time: 50.18 ms, Memory: 94.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(String s, int n) {
+        String answer = "";
+        int N = 0;
+        
+        for(int i = 0; i < s.length(); i++){
+            N = s.charAt(i) + n;
+            
+            if(s.charAt(i) == ' ') { // 공백일 경우 공백 저장
+                answer += s.charAt(i);
+                
+            } else if(s.charAt(i) < 91 && N > 90 || N > 122) { // 해당 문자가 대문자일 때 연산 후 Z 나 z를 넘어갈 경우 -> - 26
+                N -= 26;
+                answer += (char) N;
+                
+            } else {
+                answer += (char) N;
+            }
+        }
+        return answer;
+    }
+}
