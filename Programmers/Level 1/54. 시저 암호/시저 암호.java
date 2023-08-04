ae5adda0196b9a95296fd76c3f6540a7cd66cd34
Joo614 <110887604+Joo614@users.noreply.github.com>
2023-08-04 23:40:21 +0900

    Update 시저 암호.java

--- Programmers/Level 1/54. 시저 암호/시저 암호.java
+++ Programmers/Level 1/54. 시저 암호/시저 암호.java
@@ -3,13 +3,13 @@ class Solution {
         String answer = "";
         int N = 0;
         
-        for(int i = 0; i < s.length(); i++){
+        for (int i = 0; i < s.length(); i++){
             N = s.charAt(i) + n;
             
-            if(s.charAt(i) == ' ') { // 공백일 경우 공백 저장
+            if (s.charAt(i) == ' ') { // 공백일 경우 공백 저장
                 answer += s.charAt(i);
                 
-            } else if(s.charAt(i) < 91 && N > 90 || N > 122) { // 해당 문자가 대문자일 때 연산 후 Z 나 z를 넘어갈 경우 -> - 26
+            } else if (s.charAt(i) < 91 && N > 90 || N > 122) { // 해당 문자가 대문자일 때 연산 후 Z 나 z를 넘어갈 경우 -> - 26
                 N -= 26;
                 answer += (char) N;
                 
