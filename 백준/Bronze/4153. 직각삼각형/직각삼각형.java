cb074ce0af4cb8567205e30155ff263dff9fa4ce
Joo614 <110887604+Joo614@users.noreply.github.com>
2024-01-01 21:15:50 +0900

    [Bronze III] Title: 직각삼각형, Time: 120 ms, Memory: 14024 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.java
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -1,14 +1,18 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Scanner in = new Scanner(System.in);
+     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         while (true) {
-            int a =  in.nextInt();
-            int b =  in.nextInt();
-            int c =  in.nextInt();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
+            int a =  Integer.parseInt(st.nextToken());
+            int b =  Integer.parseInt(st.nextToken());
+            int c =  Integer.parseInt(st.nextToken());
 
             if(a == 0 && b == 0 && c ==0) break;
 
