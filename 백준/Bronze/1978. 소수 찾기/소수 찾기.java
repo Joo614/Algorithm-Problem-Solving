b9bc4f2c0592621d0c8fd9a50aff830cd790e496
Joo614 <110887604+Joo614@users.noreply.github.com>
2024-01-18 00:31:42 +0900

    [Bronze II] Title: 소수 찾기, Time: 124 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		br.readLine(); // N 은 쓰지 않음.
+
+		int count = 0;
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " "); // 공백 기준으로 끊어줌
+
+		while (st.hasMoreTokens()) { // st.hasMoreTokens() : StringTokenizer에 사용할 수 있는 토큰이 더 있는지 확인ㄴ
+			int num = Integer.parseInt(st.nextToken());
+			
+			if (isPrime(num)) {
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+
+
+	public static boolean isPrime(int num) {
+		if (num == 1)
+			return false;
+
+		for (int i = 2; i <= Math.sqrt(num); i++) {
+			if (num % i == 0)
+				return false;
+		}
+
+		return true;
+	}
+}
