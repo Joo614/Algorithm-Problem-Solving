826957db51f733877dc125f8b9eac7f91e845c2e
Joo614 <110887604+Joo614@users.noreply.github.com>
2024-01-21 04:32:40 +0900

    Update 수 정렬하기 3.java

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -11,7 +10,6 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
 
         int[] arr = new int[N];
-
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
