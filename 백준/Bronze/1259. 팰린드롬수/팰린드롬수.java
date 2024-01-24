5b8ee2fcb9040fe8ae59678fb1958a78b06b338e
Joo614 <110887604+Joo614@users.noreply.github.com>
2024-01-25 01:55:43 +0900

    Update 팰린드롬수.java

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -3,16 +3,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+   public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        while (true) {
+        while (true) { // 무한루프
             String num = br.readLine(); // 숫자 입력 받기
 
             StringBuilder sb = new StringBuilder(num);
-            String reverseNum = sb.reverse().toString();
+            String reverseNum = sb.reverse().toString(); // 여기서 sb는 StringBuilder 타입이라서 toString()
 
-            if(num.equals("0")) break;
+            if(num.equals("0")) break; // 입력 마지막 줄에 0이 주어지니까 --> 그럼 무한루프 탈출
 
             if (num.equals(reverseNum)) {
                 System.out.println("yes");
