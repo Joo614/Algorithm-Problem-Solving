d05723b0ac12ba7a946b65cf27224fcd8dbec67d
Joo614 <110887604+Joo614@users.noreply.github.com>
2024-01-28 02:34:02 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 128 ms, Memory: 14592 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -18,7 +18,7 @@ public class Main {
         }
 
         String str = big.toString();
-        for (int i = str.length() - 1; i > 1; i--) { // 뒤부터 자릿수 확인
+        for (int i = str.length() - 1; i >= 0; i--) { // 뒤부터 자릿수 확인
             if (str.charAt(i) - '0' == 0) count++; // 0이 맞으면 count++
             else break;
         }
