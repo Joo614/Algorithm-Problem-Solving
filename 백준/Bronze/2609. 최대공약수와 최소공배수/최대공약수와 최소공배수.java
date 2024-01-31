a1cee517c0f1f847e4dddc5af90d75d8ebf089bb
Joo614 <110887604+Joo614@users.noreply.github.com>
2024-01-31 15:17:33 +0900

    Update 최대공약수와 최소공배수.java

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -26,3 +26,38 @@ public class Main {
         System.out.println(least);
     }
 }
+
+/////// 다른 풀이 ///////
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        // 쵀대공약수
+        int greatest = euclidean(a, b);
+
+        // 최소공배수
+        int least = (a * b) / greatest;
+
+        System.out.println(greatest);
+        System.out.println(least);
+    }
+
+    public static int euclidean(int a, int b) { // 유클리드 호제법
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
