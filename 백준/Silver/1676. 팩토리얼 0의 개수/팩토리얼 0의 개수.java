6ae8209f2ca634a2bf65bb1513a226ac528aef41
Joo614 <110887604+Joo614@users.noreply.github.com>
2024-01-31 13:47:26 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 128 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -10,18 +10,9 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         int count = 0; // 0의 개수 카운트
 
-        BigInteger big = new BigInteger("1"); // 객체 생성 후 1로 초기화 --> 문자열 "1"을 가지는 BigInteger 객체 생성
-        // 이후 이 객체에 다른 객체값을 곱해줄 것이기 때문에 0이 아닌, 1로 초기화
-
-        // N! 계산
-        for (int i = 1; i <= N; i++) {
-            big = big.multiply(BigInteger.valueOf(i)); // 객체끼리의 연산이기 때문에 *= 사용 불가
-        }
-
-        String str = big.toString();
-        for (int i = str.length() - 1; i > 1; i--) { // 뒤부터 자릿수 확인
-            if (str.charAt(i) == '0') count++;
-            else break;
+        while(N >= 5) {
+            count += N/5;
+            N /= 5;
         }
 
         System.out.println(count);
