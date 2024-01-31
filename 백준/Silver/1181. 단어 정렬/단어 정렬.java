d2a83cbcb00dd900afa960f3ab578de1580abb97
Joo614 <110887604+Joo614@users.noreply.github.com>
2024-01-31 16:18:54 +0900

    Update 단어 정렬.java

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -34,3 +34,32 @@ public class Main {
         System.out.println(sb);
     }
 }
+
+/////// 다른 풀이 ///////
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+
+public class No_1181 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine()); // 단어의 개수
+
+        String[] result = br.lines() // 각 줄을 스트림으로 변환
+                .distinct() // 중복 제거
+                .sorted(Comparator.comparing(String::length) // 길이에 대해 정렬
+                        .thenComparing(Comparator.naturalOrder())) // 길이가 같은 경우, 사전 순으로 정렬
+                .toArray(String[]::new); // 배열로 변환
+
+
+        for (String word : result) {
+            sb.append(word).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
