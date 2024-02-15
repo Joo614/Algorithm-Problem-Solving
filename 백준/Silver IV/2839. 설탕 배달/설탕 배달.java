e8cd046d5d5e90fbac85424ba88755d374d34764
Joo614 <110887604+Joo614@users.noreply.github.com>
2024-02-15 22:07:20 +0900

    Update 설탕 배달.java

--- 백준/Silver IV/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver IV/2839. 설탕 배달/설탕 배달.java
@@ -10,22 +10,19 @@ public class Main {
         int count = 0;
 
         // 5로 나누어질 때까지 3으로 뺌
-        // N을 3 이상으로 한 것은 --> 어차피 N이 2나 1이면 -1 출력해야하니까
         while (N > 0) {
 
             if (N % 5 == 0) {
                 count += N / 5;
                 System.out.println(count);
-                return;
-            } else if (N < 3) {
+                break;
+            } else if (N < 3) { // 어차피 N이 2나 1이면 -1 출력해야하니까
                 System.out.println(-1);
-                return;
+                break;
             }
 
             N -= 3;
             count ++;
         }
-        
-        System.out.println(count);
     }
 }
