34f7607ac61d3a559d78b50fd04508d1e0228a3f
Joo614 <110887604+Joo614@users.noreply.github.com>
2024-02-15 22:05:12 +0900

    [Silver IV] Title: 설탕 배달, Time: 120 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver IV/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver IV/2839. 설탕 배달/설탕 배달.java
@@ -1,28 +1,31 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        Scanner sc = new Scanner(System.in);
-        int sugar = sc.nextInt();
-
+        int N = Integer.parseInt(br.readLine());
         int count = 0;
-        while(sugar > 0) {
-            if(sugar % 5 == 0) {
-                count += sugar / 5;
-                System.out.println(count);
-                return ;
-            }
-            if(sugar < 3) {
-                System.out.println(-1);
-                return ;
-            }
-            sugar-=3;
-            count++;
-        }
 
-        System.out.println(count);
+        // 5로 나누어질 때까지 3으로 뺌
+        // N을 3 이상으로 한 것은 --> 어차피 N이 2나 1이면 -1 출력해야하니까
+        while (N > 0) {
+
+            if (N % 5 == 0) {
+                count += N / 5;
+                System.out.println(count);
+                return;
+            } else if (N < 3) {
+                System.out.println(-1);
+                return;
+            }
+
+            N -= 3;
+            count ++;
+        }
         
+        System.out.println(count);
     }
 }
