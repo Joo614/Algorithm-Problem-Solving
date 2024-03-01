262cf9b71fe3a866acbbe2f7de1b8ca5a4925f94
Joo614 <110887604+Joo614@users.noreply.github.com>
2024-03-01 10:37:41 +0900

    [Silver II] Title: 스택 수열, Time: 332 ms, Memory: 27196 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -5,41 +5,35 @@ import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();	// 출력할 결과물 저장
-		
-		Stack<Integer> stack = new Stack<>();
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		int start = 0;
-		
-		// N 번 반복
-		while(N -- > 0) {
-			
-			int value = Integer.parseInt(br.readLine());
-			
-			if(value > start) {
-				// start + 1부터 입력받은 value 까지 push를 한다.
-				for(int i = start + 1; i <= value; i++) {
-					stack.push(i);
-					sb.append('+').append('\n');	// + 를 저장한다. 
-				}
-				start = value; 	// 다음 push 할 때의 오름차순을 유지하기 위한 변수 초기화 
-			}
-			
-			// top에 있는 원소가 입력받은 값과 같이 않은 경우  
-			else if(stack.peek() != value) {
-				System.out.println("NO");
-				return;		// 또는 System.exit(0); 으로 대체해도 됨. 
-			}
-			
-			stack.pop();
-			sb.append('-').append('\n');
-			
-		}
-		
-		System.out.println(sb);
-	}
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        Stack<Integer> stack = new Stack<>();
+
+        int N = Integer.parseInt(br.readLine());
+
+        int start = 0; // 스택에 넣은 위치를 저장하기 위함
+
+        while (N-- > 0) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num > start) { // start +1부터 입력받은 num 까지 push
+                for (int i = start + 1; i <= num; i++) {
+                    stack.push(i);
+                    sb.append('+').append('\n');
+                }
+
+                start = num; // 다음 push 할 때의 오름차순을 유지하기 위한 변수 초기화
+            }
+            else if (stack.peek() != num) { // 맨 위에 있는 원소가 입력받은 값과 같지 않은 경우
+                System.out.println("NO");
+                return;
+            }
+
+            stack.pop();
+            sb.append('-').append('\n');
+        }
+
+        System.out.println(sb);
+    }
 }
