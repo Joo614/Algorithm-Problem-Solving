1031797e03df207ea13551dc7b5ffc77aa5ae921
Joo614 <110887604+Joo614@users.noreply.github.com>
2024-03-12 12:00:35 +0900

    [Bronze II] Title: 음계, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.java
+++ 백준/Bronze/2920. 음계/음계.java
@@ -11,27 +11,24 @@ public class Main {
 
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-		int[] originalArr = new int[8];
-		int[] sortedArr = new int[8];
-		Integer[] reverseSortedArr = new Integer[8];
+		int[] arr = new int[8];
 
-		// 세 배열에 모두 넣어주고
+		// 배열에 넣고
 		for (int i = 0; i < 8; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			originalArr[i] = num;
-			sortedArr[i] = num;
-			reverseSortedArr[i] = num;
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 
-		Arrays.sort(sortedArr);
-		Arrays.sort(reverseSortedArr, Comparator.reverseOrder());
-		int[] reversSortedArr = Arrays.stream(reverseSortedArr)
-			.mapToInt(Integer::intValue)
-			.toArray();
+		boolean ascending = true;
+		boolean descending = true;
 
-		Boolean ascending = Arrays.equals(originalArr, sortedArr);
-		Boolean descending = Arrays.equals(originalArr, reversSortedArr);
-		
+		for (int i = 0; i < 7; i++) {
+			if (arr[i] < arr[i + 1]) {
+				descending = false; // 뒤의 요소가 더 크면 내림차순 false
+			} else if (arr[i] > arr[i + 1]) {
+				ascending = false; // 앞의 요소가 더 크면 오름차순 false
+			}
+		}
+
 		if (ascending) {
 			System.out.println("ascending");
 		} else if (descending) {
